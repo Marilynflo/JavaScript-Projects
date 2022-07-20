bded0a20241b452d2cae7c44c49b7661e2b6e62f
Marilynflo <107453088+Marilynflo@users.noreply.github.com>
2022-07-19 23:10:47 -0700

    Add files via upload

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,32 @@
+
+        window.alert("Flores Family Tree");
+
+
+         var M = "Marilyn" ; //created a variable called M
+        var M = M.fontcolor("Green"); //assigned variable the color green
+        document.write(M)
+   
+    
+        var D = "Dennis" ;
+        var L = "Dancel" ;
+        document.write (D+L);
+
+        var I = "Irvin, Ira , Iggy";
+        document.write(I);
+
+        var Z= "Flores" + "Family";
+        document.write(Z)
+
+        document.write(422+1560);
+
+        function My_First_Function() {
+            var str = "I love green color";
+            var result = str.fontcolor("green");
+            document.getElementById("Green_Text").innerHTML = result;
+        }
+
+
+
+
+
+
