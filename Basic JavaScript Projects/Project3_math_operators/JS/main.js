1ff9a178b3f0f3100862d21a950cf79789626c97
Marilynflo <107453088+Marilynflo@users.noreply.github.com>
2022-07-27 07:39:03 -0700

    Add files via upload

---
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -0,0 +1,42 @@
+
+{
+    window.alert(Math.random()); // Usinga alert window to pop up random numbers
+}
+function addition_Function (){// Defining a function and naming it
+    var addition = 1982+39; // Defining a variable and giving it a value
+    document.getElementById("Math").innerHTML = "1982-39= " + addition; // method returns an element with a specified value
+}
+function subtraction_Function() { // Defining a function and naming it
+    var subtraction = 5-2; // Defining a variable and giving it a value
+   
+    document.getElementById("Math").innerHTML = "5-2 = " + subtraction; // method returns an element with specified value
+}
+
+function multiplication () {// Defining a function and naming it
+    var simple_math =  9 * 8 ;// Defining a variable and giving it a value
+    document.getElementById("Math").innerHTML = "9 * 8 =" + simple_math;// method returns an element with specified value
+}
+
+function division () {// defining a function and naming it
+    var simple_math = 8/68; // defining a variable and giving it a value
+    document.getElementById("Math").innerHTML ="8/ 68=" + simple_math; // method returns an element with specified value
+}
+
+function more_Math () { // defining a function and naming it
+    var simple_Math = (19 + 82) * 8 / 2-5; //defining a variable and giving it a value
+    document.getElementById("Math").innerHTML = "19 plus 82, multiple by 8, divided in half and then subtracted by 5 equals " + simple_Math;// method retunrs an element with specified value
+}
+
+function modulus_Operator() { // defining a function and naming it
+    var simple_math = 25%6; // defining a variable and giving it a value
+    document.getElementById("Math").innerHTML = "When you divide 25 by 6 you have remainder of:" + simple_math;// method retunrs an element with specified value
+}
+
+function negation_Operator() {// defining a function and naming it
+    var x = 50; // defining a variable and giving it a value
+    document.getElementById("Math").innerHTML = -x; // method returns an element with specified value
+
+}
+
+
+
