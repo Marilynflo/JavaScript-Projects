ef20f5fd6717a3bf22fbd4377ab9792d5dec2dc7
Marilynflo <107453088+Marilynflo@users.noreply.github.com>
2022-07-27 09:00:42 -0700

    Add files via upload

---
+++ Basic JavaScript Projects/Project4_dictionaries/Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,6 @@
+
+    function My_First_Function() { // Defining a function and naming it
+        var sentence = "My JavaScript code"; // Defining a variable and giving it a value
+        sentence += " is a lot complicated than the previous";
+        document.getElementById("Assignment").innerHTML = sentence;
+    }
